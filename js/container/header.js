7fd0818ca84a15515111be12d9f12f056963da40
Wagner da silva junior <wagnerjr306@gmail.com>
2025-10-07 19:46:20 -0300

    Crie o header tanto no html com no css, criei a pasta de pdf para guardar os pdf do projeto, criei um arquivo javaScript para importar o header e crie uma logo.

---
+++ js/container/header.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded",function () {
+    const header = `
+    <header>
+        <div class="header">
+
+            <div class="logoHeader">
+                <a href="index.html"><img src="img/logoHeader.png" alt="logo-header"></a>
+            </div>
+
+            <div class="nav">
+                <nav>
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="#">Personagens</a></li>
+                        <li><a href="#">Boss</a></li>
+                        <li><a href="#">Armas</a></li>
+                        <li><a href="#">Rotas</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+    </header>
+    <hr>
+    `;
+    document.body.insertAdjacentHTML("afterbegin",header);
+});
